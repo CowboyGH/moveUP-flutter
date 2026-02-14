1fcb83a81de7875c93c36ff7a5d16cc92fb21440
CowboyGH <kocergindenis81@gmail.com>
2026-02-14 10:57:39 +0700

    chore(locale): remove localization files and related dependencies

--- lib/features/app/app.dart
+++ lib/features/app/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../core/localization/generated/app_localizations.g.dart';
 import '../../core/router/router.dart';
 
 /// The main application widget.
@@ -12,8 +11,6 @@ class FlutterStarterTemplate extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: router,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
     );
   }
 }
