c4e0876417facd3f0b918392a59962ffcc987162
CowboyGH <kocergindenis81@gmail.com>
2026-03-02 17:11:55 +0700

    feat(auth): update UserDto to include avatar and email verification fields

--- lib/features/auth/data/dto/user_dto.dart
+++ lib/features/auth/data/dto/user_dto.dart
@@ -2,8 +2,8 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'user_dto.g.dart';
 
-/// DTO for User information.
-@JsonSerializable()
+/// DTO for user information.
+@JsonSerializable(createToJson: false)
 class UserDto {
   /// Unique identifier for the user.
   final int id;
@@ -14,9 +14,16 @@ class UserDto {
   /// Email address of the user.
   final String email;
 
+  /// Avatar of the user.
+  final String avatar;
+
   /// Role identifier for the user.
   @JsonKey(name: 'role_id')
   final int roleId;
+
+  /// Timestamp of the user email verification.
+  @JsonKey(name: 'email_verified_at')
+  final String emailVerifiedAt;
 
   /// Timestamp of the last update to the user information.
   @JsonKey(name: 'updated_at')
@@ -31,7 +38,9 @@ class UserDto {
     required this.id,
     required this.name,
     required this.email,
+    required this.avatar,
     required this.roleId,
+    required this.emailVerifiedAt,
     required this.updatedAt,
     required this.createdAt,
   });
