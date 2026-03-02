df074f137b5b51e868547546e12e1a79b90c3fd4
CowboyGH <kocergindenis81@gmail.com>
2026-03-02 16:26:58 +0700

    refactor(logging): improve URI logging by using a safe path method

--- lib/core/network/interceptors/logging_interceptor.dart
+++ lib/core/network/interceptors/logging_interceptor.dart
@@ -10,16 +10,21 @@ final class LoggingInterceptor extends InterceptorsWrapper {
   /// Creates an instance of [LoggingInterceptor].
   LoggingInterceptor(this.logger);
 
+  String _safePath(Uri uri) {
+    final path = uri.path.isEmpty ? '/' : uri.path;
+    return uri.query.isEmpty ? path : '$path?<REDACTED>';
+  }
+
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    logger.d('-> ${options.method} ${options.uri}');
+    logger.d('-> ${options.method} ${_safePath(options.uri)}');
     return handler.next(options);
   }
 
   @override
   void onResponse(Response<dynamic> response, ResponseInterceptorHandler handler) {
     logger.d(
-      '<- ${response.statusCode} ${response.requestOptions.method} ${response.requestOptions.uri}',
+      '<- ${response.statusCode} ${response.requestOptions.method} ${_safePath(response.requestOptions.uri)}',
     );
     return handler.next(response);
   }
@@ -28,7 +33,7 @@ final class LoggingInterceptor extends InterceptorsWrapper {
   void onError(DioException err, ErrorInterceptorHandler handler) {
     final request = err.requestOptions;
     logger.w(
-      'x ${err.response?.statusCode ?? '-'} ${err.type} ${request.method} ${request.uri} ${err.message ?? ''}',
+      'x ${err.response?.statusCode ?? '-'} ${err.type} ${request.method} ${_safePath(request.uri)}',
     );
     return handler.next(err);
   }
