58d153d19f0fe7831b977110c47d59ef437ce623
CowboyGH <kocergindenis81@gmail.com>
2026-03-09 13:30:22 +0700

    refactor(auth): remove redundant fromJson method from LoginRequestDto

--- lib/features/auth/data/dto/login_request_dto.dart
+++ lib/features/auth/data/dto/login_request_dto.dart
@@ -3,7 +3,7 @@ import 'package:json_annotation/json_annotation.dart';
 part 'login_request_dto.g.dart';
 
 /// DTO for the login request.
-@JsonSerializable()
+@JsonSerializable(createFactory: false)
 class LoginRequestDto {
   /// User email.
   final String email;
@@ -13,9 +13,6 @@ class LoginRequestDto {
 
   /// Creates an instance of [LoginRequestDto].
   LoginRequestDto({required this.email, required this.password});
-
-  /// Creates a [LoginRequestDto] from JSON.
-  factory LoginRequestDto.fromJson(Map<String, dynamic> json) => _$LoginRequestDtoFromJson(json);
 
   /// Converts [LoginRequestDto] to JSON.
   Map<String, dynamic> toJson() => _$LoginRequestDtoToJson(this);
