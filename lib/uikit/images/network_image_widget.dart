c32b311e272c1f3397e00b8e22b204d3c1b7c2b0
CowboyGH <kocergindenis81@gmail.com>
2026-03-10 17:23:06 +0700

    feat(uikit): add NetworkImageWidget for cached network images

---
+++ lib/uikit/images/network_image_widget.dart
@@ -0,0 +1,50 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import '../themes/colors/app_color_theme.dart';
+
+/// Widget for displaying cached network images.
+class NetworkImageWidget extends StatelessWidget {
+  /// Image URL.
+  final String imageUrl;
+
+  /// Height.
+  final double height;
+
+  /// Creates an instance of [NetworkImageWidget].
+  const NetworkImageWidget({
+    required this.imageUrl,
+    required this.height,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      child: CachedNetworkImage(
+        imageUrl: imageUrl,
+        fit: BoxFit.cover,
+        placeholder: (_, _) => const _ImagePlaceholder(),
+        errorWidget: (_, _, _) => const _ImagePlaceholder(),
+      ),
+    );
+  }
+}
+
+class _ImagePlaceholder extends StatelessWidget {
+  const _ImagePlaceholder();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Center(
+        child: Icon(
+          Icons.image_outlined,
+          size: 64,
+          color: AppColorTheme.of(context).disabled,
+        ),
+      ),
+    );
+  }
+}
