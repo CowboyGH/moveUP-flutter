d1f50cc9a699137e1ab7f0ee25bb671d07237f2c
CowboyGH <kocergindenis81@gmail.com>
2026-03-26 15:10:18 +0700

    test(workouts): add unit tests for WorkoutsOverviewCubit

---
+++ test/features/workouts/overview/presentation/cubits/workouts_overview_cubit_test.dart
@@ -0,0 +1,86 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:moveup_flutter/core/failures/feature/workouts/workouts_failure.dart';
+import 'package:moveup_flutter/core/result/result.dart';
+import 'package:moveup_flutter/features/workouts/overview/domain/entities/workout_overview_item.dart';
+import 'package:moveup_flutter/features/workouts/overview/domain/repositories/workouts_overview_repository.dart';
+import 'package:moveup_flutter/features/workouts/overview/presentation/cubits/workouts_overview_cubit.dart';
+
+import 'workouts_overview_cubit_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<WorkoutsOverviewRepository>()])
+void main() {
+  late MockWorkoutsOverviewRepository repository;
+  late WorkoutsOverviewCubit workoutsOverviewCubit;
+
+  const items = [
+    WorkoutOverviewItem(
+      userWorkoutId: 1,
+      workoutId: 1,
+      title: 'title',
+      description: 'description',
+      durationMinutes: 1,
+      imageUrl: 'test.png',
+      status: 'started',
+    ),
+  ];
+
+  setUp(() {
+    repository = MockWorkoutsOverviewRepository();
+    workoutsOverviewCubit = WorkoutsOverviewCubit(repository);
+    provideDummy<Result<List<WorkoutOverviewItem>, WorkoutsFailure>>(
+      const Success<List<WorkoutOverviewItem>, WorkoutsFailure>(items),
+    );
+  });
+
+  group('WorkoutsOverviewCubit', () {
+    const workoutsFailure = WorkoutsRequestFailure('error_message');
+
+    blocTest<WorkoutsOverviewCubit, WorkoutsOverviewState>(
+      'emits inProgress only once when loadWorkouts is called twice',
+      setUp: () => when(repository.getWorkouts()).thenAnswer(
+        (_) async => const Success<List<WorkoutOverviewItem>, WorkoutsFailure>(items),
+      ),
+      build: () => workoutsOverviewCubit,
+      act: (cubit) {
+        cubit.loadWorkouts();
+        cubit.loadWorkouts();
+      },
+      expect: () => const [
+        WorkoutsOverviewState.inProgress(),
+        WorkoutsOverviewState.loaded(items),
+      ],
+      verify: (_) => verify(repository.getWorkouts()).called(1),
+    );
+
+    blocTest<WorkoutsOverviewCubit, WorkoutsOverviewState>(
+      'emits loaded(items) when load-workouts is succeed',
+      setUp: () => when(repository.getWorkouts()).thenAnswer(
+        (_) async => const Success<List<WorkoutOverviewItem>, WorkoutsFailure>(items),
+      ),
+      build: () => workoutsOverviewCubit,
+      act: (cubit) => cubit.loadWorkouts(),
+      expect: () => const [
+        WorkoutsOverviewState.inProgress(),
+        WorkoutsOverviewState.loaded(items),
+      ],
+      verify: (_) => verify(repository.getWorkouts()).called(1),
+    );
+
+    blocTest<WorkoutsOverviewCubit, WorkoutsOverviewState>(
+      'emits failed(workoutsFailure) when load-workouts is failed',
+      setUp: () => when(repository.getWorkouts()).thenAnswer(
+        (_) async => const Failure<List<WorkoutOverviewItem>, WorkoutsFailure>(workoutsFailure),
+      ),
+      build: () => workoutsOverviewCubit,
+      act: (cubit) => cubit.loadWorkouts(),
+      expect: () => const [
+        WorkoutsOverviewState.inProgress(),
+        WorkoutsOverviewState.failed(workoutsFailure),
+      ],
+      verify: (_) => verify(repository.getWorkouts()).called(1),
+    );
+  });
+}
